686581d88c557bd8f38db0921a745f9a423739ba
SJD39 <sanjiudao@qq.com>
2025-01-13 22:24:15 +0800

    25.1.13
    25.1.13

--- element/CoilShow.js
+++ element/CoilShow.js
@@ -6,14 +6,26 @@ class CoilShow extends HTMLElement {
     }
 
     connectedCallback() {
+        this.runMode = 0;
         this.station;
         this.addr;
 
-        this.coilShowBox = document.createElement('div');
         this.className = 'coilShowBox';
-        this.innerText = 'False';
 
-        this.append(this.coilShowBox);
+        this.coilShowValue = document.createElement('div');
+        this.coilShowValue.innerText = 'False';
+
+        this.coilShowDel = document.createElement('div');
+        this.coilShowDel.innerText = '删除';
+        this.coilShowDel.style.color = 'red';
+        this.coilShowDel.addEventListener('click', () => {
+            this.remove();
+        });
+
+        this.coilShowInfo = document.createElement('div');
+        this.coilShowInfo.innerText = `01,${this.station},${this.addr}`;
+
+        this.append(this.coilShowValue, this.coilShowInfo, this.coilShowDel);
 
         // 窗口拖动
         this.draging = false;
@@ -23,25 +35,28 @@ class CoilShow extends HTMLElement {
         this.offsetY = 0;
 
         this.addEventListener('mousedown', (e) => {
+            if(this.runMode != 0){
+                return;
+            }
             this.draging = true;
             this.offsetX = this.mouseX - this.offsetLeft;
             this.offsetY = this.mouseY - this.offsetTop;
 
             this.drag();
-        })
+        });
 
         this.addEventListener('click', (e) => {
             e.stopImmediatePropagation();
-        })
+        });
 
         document.addEventListener('mouseup', (e) => {
             this.draging = false;
-        })
+        });
 
         document.addEventListener('mousemove', (e) => {
             this.mouseX = e.clientX;
             this.mouseY = e.clientY;
-        })
+        });
     }
 
     // 拖动动画
@@ -54,6 +69,16 @@ class CoilShow extends HTMLElement {
                 this.drag();
             });
         }
+    }
+
+    toRunMode() {
+        this.coilShowDel.style.display = 'none';
+        this.runMode = 1;
+    }
+
+    toEditorMode() {
+        this.coilShowDel.style.display = 'block';
+        this.runMode = 0;
     }
 }
 
