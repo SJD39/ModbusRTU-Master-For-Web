bc9fff83d1f5c68735489377fcf338b68ce0b388
SJD39 <sanjiudao@qq.com>
2025-03-14 00:00:32 +0800

    零时

--- js/ModBusRTUMaster.js
+++ js/ModBusRTUMaster.js
@@ -10,128 +10,16 @@ class ModBusRTUMaster {
         this.analysisEn = false;
     }
 
-    // 添加任务队列
-    enqueueTask(task) {
-        this.taskQueue.push(task);
-    }
-
-    // 执行任务队列
-    async executeQueue() {
-        console.log("当前任务数：", this.taskQueue.length);
-
-        if (this.taskRunning || this.taskQueue.length === 0) {
-            setTimeout(async () => { this.executeQueue() }, 0);
-            return;
-        }
-
-        this.taskRunning = true;
-        console.log("任务执行中。。。");
-        const task = this.taskQueue.shift();
-
-        try {
-            await task();
-        } catch (error) {
-            console.log("寄了:", error);
-        } finally {
-            this.taskRunning = false;
-            console.log("任务执行完成。。");
-            setTimeout(async () => { this.executeQueue() }, 0);
-        }
-    }
-
-    // 串口读取
-    async serialRead(id, funCode) {
-        // 获取读取器
-        let reader;
-        try {
-            reader = this.port.readable.getReader();
-        } catch (error) {
-            console.log("获取读取错误");
-            return;
-        }
-
-        // 以下为modbus主站响应实现
-        let first = true;
-        let data = [];
-        let dataLen = 0;
-        // 已处理数据计数
-        let dataCount = 0;
-        let step = 0;
-        let result = [];
-        while (true) {
-            const { value, done } = await reader.read();
-
-            // 扔掉之前接受到的数据
-            // if (!first) {
-            //     console.log(`丢掉~${value}`);
-            //     break;
-            // }
-
-            // 串口数据
-            for (let i = 0; i < value.length; i++) {
-                data.push(value[i]);
-            }
-            console.log(value);
-            console.log(data);
-
-            // ModBus指令解析
-            if (step == 0) {
-                funCode = data[dataCount];
-                if (!this.funCodes.includes(funCode)) {
-                    console.log(`异常的功能码 ${funCode}`);
-                    return { "error": "异常的功能码" };
-                }
-
-                console.log(`功能码：${funCode}`);
-                dataCount++;
-                step++;
-            }
-
-            if (step == 1 && (data.length - dataCount > 0)) {
-                if ([1, 2, 3, 4].includes(funCode)) {
-                    dataLen = data[dataCount];
-                    console.log(`数据长度：${data[dataCount]}`);
-
-                    dataCount++;
-                    step++;
-                } else if ([5, 6, 15, 16].includes(funCode)) {
-                    if (data.length - dataCount >= 4) {
-                        console.log(`输出地址：${data[dataCount]}，${data[dataCount + 1]}`);
-                        console.log(`输出值：${data[dataCount + 2]}，${data[dataCount + 3]}`);
-
-                        dataCount = dataCount + 4;
-                        step = 10;
-                    }
-                }
-            }
-
-            if (step == 2 && (data.length - dataCount >= dataLen)) {
-                console.log(`数据：`);
-                for (let i = 0; i < dataLen; i++) {
-                    console.log(`${data[dataCount]}`);
-                    dataCount++;
-                }
-                step = 10;
-            }
-
-            // crc校验
-            if (step == 10 && (data.length - dataCount >= 2)) {
-                let crc = this.crc(data.slice(0, data.length - 2));
-
-                if (this.arrayEqual(crc, [data[dataCount], data[dataCount + 1]])){
-                    console.log("校验成功！");
-                }else{
-                    console.log("校验失败！");
-                }
-            }
-
-        }
-        reader.releaseLock();
-    }
-
     // 01 读线圈
     async readCoilsAsync(id, addr, length) {
-        let error = false;
+        while (this.taskRunning) {
+            console.log("等待中。。。");
+            await new Promise(resolve => setTimeout(resolve, 10));
+        }
+
+        console.log("开始执行");
+        this.taskRunning = true;
+
         // 写指令
         let addrH = addr >> 8;
         let addrL = addr & 0xFFFF;
@@ -146,62 +34,101 @@ class ModBusRTUMaster {
         writer.releaseLock();
 
         // 读返回值
+        let result = await this.serialRead(id, 1);
+        this.taskRunning = false;
+        return result;
+    }
+
+    // 串口读取
+    async serialRead(id, funCode) {
+        // 获取读取器
         let reader;
         try {
             reader = this.port.readable.getReader();
         } catch (error) {
+            console.log("获取读取错误");
             return;
         }
 
-        let readValues = [];
-        let startTime = Date.now();
+        // 以下为modbus主站响应实现
+        let step = 0;
+        let data = [];
+        let dataLen;
+        let dataCount = 0;
+        let result = [];
         while (true) {
             const { value, done } = await reader.read();
 
+            // 串口数据
             for (let i = 0; i < value.length; i++) {
-                readValues.push(value[i]);
+                data.push(value[i]);
             }
+            console.log(data);
 
-            if (readValues.length >= 3) {
-                if (readValues.length == readValues[2] + 5) {
-                    reader.releaseLock();
-                    break;
+            // ModBus指令解析
+            try {
+                // 解析站号
+                if (step == 0) {
+                    if (id != data[dataCount]) {
+                        console.log(`站号错误：${data[dataCount]}`);
+                        throw new Error("站号错误");
+                    }
+                    console.log(`站号：${data[dataCount]}`);
+
+                    dataCount++;
+                    step++;
                 }
-            }
 
-            // 超时
-            if (Date.now() - startTime > 1000) {
+                // 解析功能码
+                if (step == 1 && (data.length - dataCount > 0)) {
+                    if (funCode != data[dataCount]) {
+                        console.log(`功能码错误： ${funCode},${data[dataCount]}`);
+                        throw new Error("功能码错误");
+                    }
+                    console.log(`功能码：${data[dataCount]}`);
+
+                    dataCount++;
+                    step++;
+                }
+
+                // 解析数据
+                if (step == 2 && (data.length - dataCount > 0)) {
+                    if ([1, 2, 3, 4].includes(funCode)) {
+                        dataLen = data[dataCount];
+                        console.log(`数据长度：${data[dataCount]}`);
+
+                        dataCount++;
+                        step++;
+                    }
+                }
+
+                if (step == 3 && (data.length - dataCount >= dataLen)) {
+                    console.log(`数据：`);
+                    for (let i = 0; i < dataLen; i++) {
+                        console.log(`${data[dataCount]}`);
+                        result.push(data[dataCount]);
+                        dataCount++;
+                    }
+                    step = 10;
+                }
+
+                // crc校验
+                if (step == 10 && (data.length - dataCount >= 2)) {
+                    let crc = this.crc(data.slice(0, data.length - 2));
+
+                    if (this.arrayEqual(crc, [data[dataCount], data[dataCount + 1]])) {
+                        console.log("校验成功！");
+                    } else {
+                        console.log("校验失败！");
+                    }
+
+                    console.log("数据读取完毕！", result);
+                    return result;
+                }
+            } finally {
                 reader.releaseLock();
-                return;
             }
         }
-
-        if (error) {
-            console.log("timeout");
-            return;
-        }
-
-        // crc校验
-        let calcCrc = this.crc(readValues.slice(0, readValues.length - 2));
-        let readCrc = readValues.slice(readValues.length - 2, readValues.length);
-        if (!this.arrayEqual(calcCrc, readCrc)) {
-            console.log("crc校验失败！");
-            return;
-        }
-
-        // 输出
-        let result = [];
-        for (let i = 0; i < readValues[2]; i++) {
-            let readValue = readValues[3 + i].toString(2).padStart(8, "0");
-            for (let ii = 7; ii >= 0; ii--) {
-                result.push(readValue[ii] == 1 ? true : false);
-                if (result.length >= length) {
-                    break;
-                }
-            }
-        }
-
-        return result;
     }
 
     // 03 读保持寄存器
