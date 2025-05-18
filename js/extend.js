f77c412a7deb7b59bfb443b004393aeb33b6363b
SJD39 <sanjiudao@qq.com>
2025-05-18 22:08:57 +0800

    1
    1

--- js/extend.js
+++ js/extend.js
@@ -17,5 +17,29 @@ class Extend {
         return true;
     }
 }
-
 var extend = new Extend();
+
+class BitConvert { 
+    constructor() {
+        this.buffer = new ArrayBuffer(4);
+        this.view = new DataView(this.buffer);
+    }
+    floatToUint32(float) {
+        this.view.setFloat32(0, float);
+        return this.view.getUint32(0);
+    }
+    uint32ToFloat(uint32) {
+        this.view.setUint32(0, uint32);
+        return this.view.getFloat32(0);
+    }
+    floatToUint16(float) {
+        this.view.setFloat32(0, float);
+        return [this.view.getUint16(0), this.view.getUint16(2)];
+    }
+    uint16ToFloat(uint16) {
+        this.view.setUint16(0, uint16[0]);
+        this.view.setUint16(2, uint16[1]);
+        return this.view.getFloat32(0);
+    }
+}
+var bitConvert  = new BitConvert();
